e797a3a3d4e933d8d438c2455fe610136fbdfaf7
ye-yo <yeyo0x0@gmail.com>
2022-03-15 22:52:27 +0900

    Chore: test App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>{{ message }}</h1>
+  <chart-pentagon
+    :user-result="userResult"
+    :enterprise-result="enterpriseResult"
+  />
+  <h1 class="tag active">{{ message }}</h1>
 </template>
 
 <script>
+import ChartPentagon from "./components/ChartPentagon.vue";
+import user from "./datas/user.json";
+import enterprise from "./datas/enterprise.json";
 export default {
+  components: { ChartPentagon },
   data() {
     return {
       message: "Hello Vue!!!",
+      userResult: Object.values(user),
+      enterpriseResult: Object.values(enterprise),
     };
   },
 };
 </script>
+<style lang="scss" scoped>
+.tag {
+  &.active {
+    background-color: black;
+  }
+}
+</style>
